e59d8947239079341a7d85aab23570cd92a45025
Jason Barker <jbbarker@mit.edu>
2019-02-28 16:02:09 -0500

    Lab 6

--- src/pCommunicationAngle_jbbarker/CommunicationAngle_jbbarker.cpp
+++ src/pCommunicationAngle_jbbarker/CommunicationAngle_jbbarker.cpp
@@ -8,6 +8,7 @@
 #include <iterator>
 #include "MBUtils.h"
 #include "CommunicationAngle_jbbarker.h"
+#include <cmath>
 
 using namespace std;
 
@@ -16,6 +17,9 @@ using namespace std;
 
 CommunicationAngle_jbbarker::CommunicationAngle_jbbarker()
 {
+  m_surface_sound_speed = 1480;
+  m_sound_speed_gradient = 0.016;
+  m_water_depth =0;
 }
 
 //---------------------------------------------------------
@@ -34,6 +38,52 @@ bool CommunicationAngle_jbbarker::OnNewMail(MOOSMSG_LIST &NewMail)
    
   for(p=NewMail.begin(); p!=NewMail.end(); p++) {
     CMOOSMsg &msg = *p;
+
+    //Register for new mail
+    string key   = msg.GetKey();
+    
+    if (key == "VEHICLE_NAME")
+      m_ownship_name = msg.GetString(); 
+    if (key == "NAV_X")
+      m_nav_x = msg.GetDouble();
+    if (key == "NAV_Y")
+      m_nav_y = msg.GetDouble();
+    if (key == "NAV_DEPTH")
+      m_nav_depth = msg.GetDouble();
+    if (key == "NAV_HEADING")
+      m_nav_heading = msg.GetDouble();
+    if (key == "NAV_SPEED")
+      m_nav_speed = msg.GetDouble(); 
+    
+    
+   
+  
+    
+    //Establish a string for collecting collaborator information
+    if (key == "COLLABORATOR_NAME")
+      m_coll_name = msg.GetString();
+      m_coll_nav_x = m_coll_name + "_NAV_X";
+      Register(m_coll_nav_x,0);
+      m_coll_nav_y = m_coll_name + "_NAV_Y";
+      Register(m_coll_nav_y,0);
+      m_coll_nav_depth = m_coll_name + "_NAV_DEPTH";
+      Register(m_coll_nav_depth,0);
+      m_coll_nav_heading = m_coll_name + "_NAV_HEADING";
+      Register(m_coll_nav_heading,0);
+      m_coll_nav_speed = m_coll_name + "_NAV_SPEED";
+      Register(m_coll_nav_speed,0);
+    //Registering for Collaborator information
+    if (key == m_coll_nav_x)
+      m_c_nav_x = msg.GetDouble();
+    if (key == m_coll_nav_y)
+      m_c_nav_y = msg.GetDouble();
+    if (key == m_coll_nav_depth)
+      m_c_nav_depth = msg.GetDouble();
+    if (key == m_coll_nav_heading)
+      m_c_nav_heading = msg.GetDouble();
+    if (key == m_coll_nav_speed)
+      m_c_nav_speed = msg.GetDouble();
+  
 
 #if 0 // Keep these around just for template
     string key   = msg.GetKey();
@@ -70,6 +120,24 @@ bool CommunicationAngle_jbbarker::OnConnectToServer()
 
 bool CommunicationAngle_jbbarker::Iterate()
 {
+  double co_z = m_surface_sound_speed/m_sound_speed_gradient;
+  double distance = sqrt(pow((m_c_nav_x - m_nav_x), 2) + pow((m_c_nav_y - m_nav_y), 2));
+  Notify("DISTANCE", doubleToString(distance));
+  //Troubleshooting Notification
+
+  double center = ((pow((co_z + m_c_nav_depth), 2)-pow((co_z + m_nav_depth), 2))/(2*distance))+(distance/2);
+  Notify("CENTER", doubleToString(center));
+  //Troubleshooting Notification
+
+  double radius = sqrt(pow(center, 2) + pow((co_z + m_nav_depth),2));
+  Notify("RADIUS", doubleToString(radius));
+  //Troubleshooting Notification
+
+  double arclength = 2*radius*asin(distance/(2*radius));
+  Notify("ARCLENGTH", doubleToString(arclength));
+  //Troubleshooting Notification
+
+  
   return(true);
 }
 
@@ -107,5 +175,12 @@ bool CommunicationAngle_jbbarker::OnStartUp()
 void CommunicationAngle_jbbarker::RegisterVariables()
 {
   // Register("FOOBAR", 0);
+  Register("VEHICLE_NAME", 0);
+  Register("COLLABORATOR_NAME", 0);
+  Register("NAV_X", 0);
+  Register("NAV_Y", 0);
+  Register("NAV_DEPTH", 0);
+  Register("NAV_HEADING", 0);
+  Register("NAV_SPEED", 0);
 }
 
--- src/pCommunicationAngle_jbbarker/CommunicationAngle_jbbarker.h
+++ src/pCommunicationAngle_jbbarker/CommunicationAngle_jbbarker.h
@@ -9,12 +9,28 @@
 #define CommunicationAngle_jbbarker_HEADER
 
 #include "MOOS/libMOOS/MOOSLib.h"
+#include <sstream>
+#include <math.h> 
+#include <string> 
+#include <cstdint>
+#include <list>
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
 
 class CommunicationAngle_jbbarker : public CMOOSApp
 {
  public:
    CommunicationAngle_jbbarker();
    ~CommunicationAngle_jbbarker();
+
+   //void setNAVX(double v){m_nav_x = v;};
+   //void setNAVY(double v){m_nav_y = v;};
+   //void setNAVZ(double v){m_nav_depth = v;};
+
+
 
  protected: // Standard MOOSApp functions to overload  
    bool OnNewMail(MOOSMSG_LIST &NewMail);
@@ -25,9 +41,44 @@ class CommunicationAngle_jbbarker : public CMOOSApp
  protected:
    void RegisterVariables();
 
+   double m_surface_sound_speed;
+   double m_sound_speed_gradient;
+   double m_water_depth;
+   
+   double m_nav_x;
+   double m_nav_y;
+   double m_nav_depth;
+   double m_nav_heading;
+   double m_nav_speed; 
+
+   double m_c_nav_x;
+   double m_c_nav_y;
+   double m_c_nav_depth;
+   double m_c_nav_heading;
+   double m_c_nav_speed;
+  
+
+   std::string m_coll_name;
+   std::string m_ownship_name;
+   std::string m_coll_nav_x;
+   std::string m_coll_nav_y;
+   std::string m_coll_nav_depth;
+   std::string m_coll_nav_heading;
+   std::string m_coll_nav_speed;
+
+   
+   double m_elev_angle;
+   double m_calc_Radius;
+   double m_calc_arclength;
+   double m_calc_r;
+   double m_calc_z;
+
+
  private: // Configuration variables
+ 	
 
  private: // State variables
+ 	
 };
 
 #endif 
