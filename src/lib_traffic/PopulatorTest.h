578a416694b3c5b49ba661c4a189eddc04d8d61f
Jason Barker <jbbarker@mit.edu>
2020-03-30 14:06:16 -0400

    30Mar

---
+++ src/lib_traffic/PopulatorTest.h
@@ -0,0 +1,14 @@
+
+#ifndef POPULATOR_TEST_HEADER
+#define POPULATOR_TEST_HEADER
+
+#include "TrafficScheme.h"
+#include <string>
+#include <vector>
+
+class PopulatorTest{
+
+protected:
+TrafficScheme my_tss;
+};
+#endif
